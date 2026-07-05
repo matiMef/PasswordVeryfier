e133865f35513bf8435ab374619d5b2bcfcb582f
MatiMed <siemaniema2115@interia.pl>
2026-07-05 19:58:34 +0200

    GUI logic
    GUI logic for password adding and deleting

--- password-checker.py
+++ password-checker.py
@@ -112,6 +112,13 @@ class JsonPassword:
         self.name = name
         self.encrypted_password = password
 
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "password": self.encrypted_password
+        }
+
 class StoredPasswords:
     def __init__(self):
         self.passwords = []
@@ -205,15 +212,31 @@ class ScrollablePasswordFrame(customtkinter.CTkScrollableFrame):
         self.grid_columnconfigure(0, weight=1)
         self.values = values
         self.checkboxes = []
-
+    
         for i, value in enumerate(self.values):
             checkbox = customtkinter.CTkCheckBox(
                 self, 
-                text=value, 
+                text=value.name, 
                 text_color="white", 
                 text_color_disabled="gray",
                 command=self.verifyState
             )
+            checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+        self.update_values(values)
+
+    def update_values(self, new_values: list):
+        for checkbox in self.checkboxes:
+            checkbox.destroy()
+        self.checkboxes.clear()
+
+        for i, value in enumerate(new_values):
+            checkbox = customtkinter.CTkCheckBox(self,
+                text=value.id, 
+                text_color="white", 
+                text_color_disabled="gray",
+                command=self.verifyState)
             checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
             self.checkboxes.append(checkbox)
 
@@ -250,16 +273,18 @@ class TimeObject:
             return False
 
 class GeneratedPasswordPanel(customtkinter.CTkToplevel):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, master, stored_passwords, on_update_callback, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.geometry("500x200")
         self.title("Generator")
         self.grid_columnconfigure(0, weight=1)
         self.lift()
         self.time = TimeObject(30)
+        self.on_update_callback = on_update_callback
 
         new_password = PasswordGenerator(32)
-
+        self.stored_passwords = stored_passwords
+    
         self.progressbar = customtkinter.CTkProgressBar(
             self,
             width=500,
@@ -319,10 +344,12 @@ class GeneratedPasswordPanel(customtkinter.CTkToplevel):
         self.update_progressbar()
 
     def save_callback(self) -> str:
-        vault = VaultHandler()
         new_password = self.label.cget("text")
-        vault.save_password(self, new_password)
-
+        self.stored_passwords.add_password("test", new_password)
+        if self.on_update_callback:
+            self.on_update_callback()
+        self.destroy()
+        
     def update_progressbar(self) -> None:
         if self.time.is_elapsed() != True:
             progress = (30 - self.time.count_time())/30
@@ -338,12 +365,15 @@ class GeneratedPasswordPanel(customtkinter.CTkToplevel):
             self.destroy()
    
 class DeletionDialog(customtkinter.CTkToplevel):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, stored_passwords, password_id, on_update_callback, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.geometry("350x120")
         self.title("Confirm")
         self.lift()
         self.grid_columnconfigure(0, weight=1)
+        self.stored_passwords = stored_passwords
+        self.password_id = password_id
+        self.on_update_callback = on_update_callback
 
         self.label = customtkinter.CTkLabel(
             self, 
@@ -387,7 +417,10 @@ class DeletionDialog(customtkinter.CTkToplevel):
             pady=(20,10))
 
     def delete_callback(self):
-        pass
+        self.stored_passwords.delete_password(self.password_id)
+        if self.on_update_callback:
+            self.on_update_callback()
+        self.destroy()
 
 class PasswordPanel(customtkinter.CTkToplevel): 
     def __init__(self, *args, **kwargs):
@@ -399,20 +432,20 @@ class PasswordPanel(customtkinter.CTkToplevel):
 
         self.gen_panel = None
         self.confirmation_dialog = None
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        vault = VaultHandler("vault.json", current_dir)
-        stored_passwords = StoredPasswords()
-        vault.create_file()
-        raw_data = vault.decrypt_and_load("password")
-        stored_passwords.set_passwords(raw_data)
-        passwords_list = stored_passwords.get_passwords()
 
-        names_list = [p.name for p in passwords_list if getattr(p, 'name', None)]
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.vault = VaultHandler("vault.json", self.current_dir)
+        self.stored_passwords = StoredPasswords()
+        self.vault.create_file()
+        raw_data = self.vault.decrypt_and_load("password")
+        self.stored_passwords.set_passwords(raw_data)
+        self.passwords_list = self.stored_passwords.get_passwords()
+        self.names_list = [p.name for p in self.passwords_list if getattr(p, 'name', None)]
         
         self.scrollable_checkbox_frame = ScrollablePasswordFrame(
             self, 
             title="Saved passwords", 
-            values=names_list)
+            values=self.passwords_list)
         self.scrollable_checkbox_frame.grid(
             row=0, 
             column=0, 
@@ -484,15 +517,28 @@ class PasswordPanel(customtkinter.CTkToplevel):
             pady=10, 
             sticky="e")
     
+        self.update_vault()
+        
+    def update_vault(self):
+        self.passwords_list = self.stored_passwords.get_passwords()
+        self.names_list = [p.name for p in self.passwords_list if getattr(p, 'name', None)]
+        self.scrollable_checkbox_frame.update_values(self.passwords_list)
+        self.vault._encrypt_file(self.passwords_list, "password")
+        
     def gen_callback(self):
         if self.gen_panel is None or not self.gen_panel.winfo_exists():
-            self.gen_panel = GeneratedPasswordPanel()
+            self.gen_panel = GeneratedPasswordPanel(
+                self, 
+                stored_passwords=self.stored_passwords, 
+                on_update_callback=self.update_vault  
+            )
         else:
             self.gen_panel.focus() 
 
     def del_callback(self):
         if self.confirmation_dialog is None or not self.confirmation_dialog.winfo_exists():
-            self.confirmation_dialog = DeletionDialog()
+            deletion_id = self.scrollable_checkbox_frame.get()
+            self.confirmation_dialog = DeletionDialog(stored_passwords=self.stored_passwords, password_id=deletion_id, on_update_callback=self.update_vault)
         else:
             self.confirmation_dialog.focus() 
 
